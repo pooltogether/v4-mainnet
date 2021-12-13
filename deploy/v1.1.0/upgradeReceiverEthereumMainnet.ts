fedbdb269862b5997212aeb86557519adc35a6a5
Kames Geraghty <kamescg@gmail.com>
2021-12-13 07:40:53 -0500

    add(deploy/upgrade): file to update receiver chain upgrades

---
+++ deploy/v1.1.0/upgradeReceiverEthereumMainnet.ts
@@ -0,0 +1,21 @@
+import { dim } from 'chalk';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { configureBeaconDeployment } from '../../helpers/upgrade/v1.1.0/configureBeaconDeployment'
+import { handleBeaconContractDeploy } from '../../helpers/upgrade/v1.1.0/handleBeaconContractDeploy'
+
+const upgradeReceiverEthereumMainnet = async (hardhat: HardhatRuntimeEnvironment) => {
+  // @ts-ignore
+  const { ethers, deployments, getNamedAccounts, } = hardhat
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
+  if (process.env.DEPLOY === 'mainnet.upgrade.v1.10.0') {
+    dim(`Deploying: Receiver Chain Ethereum Mainnet`)
+    dim(`Version: 1.0.1 to 1.1.0`)
+  } else { return }
+  await handleBeaconContractDeploy(deploy, deployer, ethers)
+  await configureBeaconDeployment(ethers)
+
+  console.log(`Transactions Generated: deploy/v1.1.0/populatedTransactions.receiver.${hardhat.network}.json`)
+}
+
+export default upgradeReceiverEthereumMainnet;
