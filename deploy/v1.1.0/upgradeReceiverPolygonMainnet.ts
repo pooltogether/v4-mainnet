3de708959f97e804bb5da47ea6e5d0aa3580570a
Kames Geraghty <kamescg@gmail.com>
2021-12-13 08:03:07 -0500

    update(core): fix names of files

--- deploy/v1.1.0/upgradeReceiverPolygonMainnet.ts
+++ deploy/v1.1.0/upgradeReceiverPolygonMainnet.ts
@@ -4,20 +4,20 @@ import saveUpgradePopulatedTransactions from '../../helpers/files/saveUpgradePop
 import { configureBeaconDeployment } from '../../helpers/upgrade/v1.1.0/configureBeaconDeployment'
 import { handleBeaconContractDeploy } from '../../helpers/upgrade/v1.1.0/handleBeaconContractDeploy'
 
-const upgradeBeaconEthereumMainnet = async (hardhat: HardhatRuntimeEnvironment) => {
+const upgradeReceiverPolygonMainnet = async (hardhat: HardhatRuntimeEnvironment) => {
   // @ts-ignore
   const { ethers, deployments, getNamedAccounts, getChainId } = hardhat
   const chainId = await getChainId();
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
-  if (process.env.DEPLOY === 'mainnet.upgrade.v1.10.0') {
-    dim(`Deploying: Beacon Chain Ethereum Mainnet`)
+  if (process.env.DEPLOY === 'polygon.upgrade.v1.10.0') {
+    dim(`Deploying: Receiver Chain Ethereum Mainnet`)
     dim(`Version: 1.0.1 to 1.1.0`)
   } else { return }
   await handleBeaconContractDeploy(deploy, deployer, ethers)
   const populatedTransactions = await configureBeaconDeployment(ethers)
-  saveUpgradePopulatedTransactions(populatedTransactions, `${__dirname}/populatedTransactions.beacon.${chainId}.json`)
-  console.log(`Transactions Generated: deploy/v1.1.0/populatedTransactions.beacon.${chainId}.json`)
+  saveUpgradePopulatedTransactions(populatedTransactions, `${__dirname}/populatedTransactions.receiver.${chainId}.json`)
+  console.log(`Transactions Generated: deploy/v1.1.0/populatedTransactions.receiver.${chainId}.json`)
 }
 
-export default upgradeBeaconEthereumMainnet;
+export default upgradeReceiverPolygonMainnet;
