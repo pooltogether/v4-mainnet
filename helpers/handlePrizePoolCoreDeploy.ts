f47d1d399763adff9b49891a62f2076a7d9f83b5
Kames Geraghty <kamescg@gmail.com>
2021-12-14 08:50:05 -0500

    update(handlePrizePoolCoreDeploy): include  aave yield source contract deploy

--- helpers/handlePrizePoolCoreDeploy.ts
+++ helpers/handlePrizePoolCoreDeploy.ts
@@ -1,5 +1,7 @@
 import { deployContract } from './deployContract'
-
+import {
+  TOKEN_DECIMALS
+} from '../src/constants';
 export interface handlePrizePoolCoreDeployConfig {
   decimals: number | string,
   drawDufferCardinality: number | string,
@@ -10,21 +12,37 @@ export async function handlePrizePoolCoreDeploy(
   deploy: Function,
   deployer: string,
   ethers: any,
+  getNamedAccounts: Function,
   decimals: number | string,
   drawDufferCardinality: number | string,
   prizeDistributionBufferCardinality: number | string,
 ) {
-  const yieldSourcePrizePool = await ethers.getContract('YieldSourcePrizePool')
+
+  let {
+    executiveTeam,
+    aUSDC,
+    aaveIncentivesController,
+    aaveLendingPoolAddressesProviderRegistry,
+  } = await getNamedAccounts();
+  const yieldSourcePrizePool = await deployContract(deploy, 'ATokenYieldSource', deployer, [
+    aUSDC,
+    aaveIncentivesController,
+    aaveLendingPoolAddressesProviderRegistry,
+    TOKEN_DECIMALS,
+    "PTaUSDCY",
+    "PoolTogether aUSDC Yield",
+    executiveTeam
+  ])
   const ticketResult = await deployContract(deploy, 'Ticket', deployer, ["Ticket", "TICK", decimals, yieldSourcePrizePool.address])
-  const prizeTierHistoryResult = await deployContract(deploy, 'PrizeTierHistory', deployer, [deployer])
-  const drawBufferResult = await deployContract(deploy, 'DrawBuffer', deployer, [deployer, drawDufferCardinality])
-  const prizeDistributionBufferResult = await deployContract(deploy, 'PrizeDistributionBuffer', deployer, [deployer, prizeDistributionBufferCardinality])
+  const prizeTierHistoryResult = await deployContract(deploy, 'PrizeTierHistory', deployer, [executiveTeam])
+  const drawBufferResult = await deployContract(deploy, 'DrawBuffer', deployer, [executiveTeam, drawDufferCardinality])
+  const prizeDistributionBufferResult = await deployContract(deploy, 'PrizeDistributionBuffer', deployer, [executiveTeam, prizeDistributionBufferCardinality])
   const drawCalculatorResult = await deployContract(deploy, 'DrawCalculator', deployer, [ticketResult.address, drawBufferResult.address, prizeDistributionBufferResult.address])
-  const prizeDistributorResult = await deployContract(deploy, 'PrizeDistributor', deployer, [deployer, ticketResult.address, drawCalculatorResult.address])
-  const prizeSplitStrategyResult = await deployContract(deploy, 'PrizeSplitStrategy', deployer, [deployer, yieldSourcePrizePool.address])
-  const reserveResult = await deployContract(deploy, 'Reserve', deployer, [deployer, ticketResult.address])
+  const prizeDistributorResult = await deployContract(deploy, 'PrizeDistributor', deployer, [executiveTeam, ticketResult.address, drawCalculatorResult.address])
+  const prizeSplitStrategyResult = await deployContract(deploy, 'PrizeSplitStrategy', deployer, [executiveTeam, yieldSourcePrizePool.address])
+  const reserveResult = await deployContract(deploy, 'Reserve', deployer, [executiveTeam, ticketResult.address])
   const prizeSplitStrategy = await ethers.getContract('PrizeSplitStrategy')
-  const drawCalculatorTimelockResult = await deployContract(deploy, 'DrawCalculatorTimelock', deployer, [deployer, drawCalculatorResult.address])
+  const drawCalculatorTimelockResult = await deployContract(deploy, 'DrawCalculatorTimelock', deployer, [executiveTeam, drawCalculatorResult.address])
   const prizeDistributionFactoryResult = await deployContract(deploy, 'PrizeDistributionFactory', deployer, [
     deployer,
     prizeTierHistoryResult.address,
