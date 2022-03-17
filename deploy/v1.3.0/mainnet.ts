5fa213b68875d23c1978e5e7c3118944cd669fcb
Pierrick Turelier <pierrick@turelier.com>
2022-03-17 18:19:12 -0500

    chore(deploy): add v1.3.0 scripts

---
+++ deploy/v1.3.0/mainnet.ts
@@ -0,0 +1,32 @@
+import { dim, red } from 'chalk';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { deployAndLog } from '../../src/deployAndLog';
+
+export default async function deployToEthereumMainnet(hardhat: HardhatRuntimeEnvironment){
+  if (process.env.DEPLOY === 'v1.3.0.mainnet') {
+    dim(`Deploying: TWAB Delegator Ethereum Mainnet`)
+    dim(`Version: 1.3.0`)
+  } else { return }
+
+  const { ethers } = hardhat;
+  const { getContract, getSigners } = ethers;
+
+  const [deployer] = await getSigners();
+
+  const prizePool = await getContract('YieldSourcePrizePool');
+  const ticket = await getContract('Ticket');
+
+  // ===================================================
+  // Deploy Contracts
+  // ===================================================
+
+  if (await prizePool.getTicket() === ticket.address) {
+    await deployAndLog('TWABDelegator', {
+      from: deployer,
+      args: ['PoolTogether Staked aUSDC Ticket', 'stkPTaUSDC', ticket.address],
+      skipIfAlreadyDeployed: true,
+    });
+  } else {
+    red('Pool ticket address does not match ticket contract address');
+  }
+}
