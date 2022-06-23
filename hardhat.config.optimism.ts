d2d9ba0617d0ece64db77e89eb65e89ab8307621
Ryan <81343914+McOso@users.noreply.github.com>
2022-06-23 16:25:58 -0400

    refactor(optimism): Update hardhat.config.optimism.ts
    Co-authored-by: Pierrick Turelier <pierrick@turelier.com>

--- hardhat.config.optimism.ts
+++ hardhat.config.optimism.ts
@@ -2,7 +2,7 @@ import config from './hardhat.config';
 import { dependencyCompiler, external } from './hardhat.config.dependencies';
 
 config.etherscan = {
-  apiKey: process.env.OP_ETHERSCAN_API_KEY,
+  apiKey: process.env.OPTIMISM_ETHERSCAN_API_KEY,
 };
 
 config.dependencyCompiler = dependencyCompiler;
